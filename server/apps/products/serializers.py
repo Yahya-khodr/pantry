73918337ad88af8d60e7eabd237e9a90e0540e6d
Yahya Khodr <yhy97kh18@gmail.com>
2022-03-19 21:16:18 +0200

    Created serializers for models

---
+++ server/apps/products/serializers.py
@@ -0,0 +1,22 @@
+
+from rest_framework import serializers
+from .models import *
+
+
+class CategoryModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CategoryModel
+        fields = '__all__'
+
+
+class ProductModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductModel
+        fields = '__all__'
+
+
+
+class ItemModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ItemModel
+        fields= '__all__'
