6450f875641526bd9d60746888cd033ed9f34ae1
Yahya Khodr <yhy97kh18@gmail.com>
2022-03-19 19:01:36 +0200

    Created Item model class

--- server/apps/products/models.py
+++ server/apps/products/models.py
@@ -1,6 +1,9 @@
 
 
 
+from statistics import mode
+from tkinter import CASCADE
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -11,17 +14,24 @@ class CategoryModel(models.Model):
 
     def __str__(self):
         return self.cat_name
-        
+
 class ProductModel(models.Model):
     code = models.CharField(max_length=100, unique=True)
     product_name = models.TextField()
     product_quantity = models.TextField()
-    product_name = models.ImageField(null= True, blank = True)
-    category = models.ForeignKey(CategoryModel, null=True, blank=True, on_delete=models.CASCADE)
+    product_image = models.ImageField(null= True, blank = True)
+    category = models.ForeignKey(CategoryModel, models.CASCADE)
 
     def __str__(self):
         return self.product_name
     
 
 
-    
+class ItemModel(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,models.CASCADE)
+    product = models.ForeignKey(ProductModel, models.CASCADE)
+    company_name = models.CharField(max_length=255)
+    expiry_date = models.DateField()
+    purchased_date = models.DateField()
+    created_date = models.DateField(auto_now_add=True)
+    update_date = models.DateField(auto_now=True)
