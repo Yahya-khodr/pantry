f649d9cccca1db7dc81ef4b64b9ceffb74171c59
Yahya Khodr <yhy97kh18@gmail.com>
2022-03-23 00:41:12 +0200

    Created categories list

---
+++ frontend/lib/utils/categories.dart
@@ -0,0 +1,32 @@
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+List categories = [
+  {
+    "name": "Drinks",
+    "icon": FontAwesomeIcons.pizzaSlice,
+  },
+  {
+    "name": "Vegetables",
+    "icon": FontAwesomeIcons.pizzaSlice,
+  },
+  {
+    "name": "Fruits",
+    "icon": FontAwesomeIcons.pizzaSlice,
+  },
+  {
+    "name": "Dairy",
+    "icon": FontAwesomeIcons.pizzaSlice,
+  },
+  {
+    "name": "Meats & Eggs",
+    "icon": FontAwesomeIcons.pizzaSlice,
+  },
+  {
+    "name": "Spices",
+    "icon": FontAwesomeIcons.pizzaSlice,
+  },
+  {
+    "name": "Others",
+    "icon": FontAwesomeIcons.pizzaSlice,
+  },
+];
