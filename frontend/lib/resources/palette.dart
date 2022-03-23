bfc5790c489740a134f7d034fd0c0939ba088f02
Yahya Khodr <yhy97kh18@gmail.com>
2022-03-23 13:12:10 +0200

    Added more colors

--- frontend/lib/resources/palette.dart
+++ frontend/lib/resources/palette.dart
@@ -2,7 +2,9 @@ import 'package:flutter/cupertino.dart';
 
 class Palette {
   static const Color appBarColor = Color(0xFF00AF54);
+  static const Color appNameColor = Color(0xFFFFE082);
   static const Color appBarColorLinear = Color(0xFF0EB177);
   static const Color unselectedIconColor = Color(0xFF767676);
   static const Color backgroundColor = Color(0xFFEDEDED);
+  static const Color textFieldColor = Color(0xFFFFFFFF);
 }
