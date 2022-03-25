1a6ba52212625f155f501f3efd7edbe5c0201cce
Yahya Khodr <yhy97kh18@gmail.com>
2022-03-25 16:32:31 +0200

    Implemented items screen with responsivness

--- frontend/lib/views/screens/items_screen.dart
+++ frontend/lib/views/screens/items_screen.dart
@@ -1,37 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/views/widgets/card_item_widget.dart';
+import 'package:frontend/views/widgets/custom_appbar_widget.dart';
 
 class ItemsScreen extends StatelessWidget {
   const ItemsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: ListView(
-        children: <Widget>[
-          const SizedBox(height: 15.0),
-          Container(
-              padding: const EdgeInsets.only(right: 15.0),
-              width: MediaQuery.of(context).size.width - 30.0,
-              height: MediaQuery.of(context).size.height - 50.0,
-              child: GridView.count(
-                crossAxisCount: 2,
-                primary: false,
-                crossAxisSpacing: 10.0,
-                mainAxisSpacing: 15.0,
-                childAspectRatio: 0.8,
-                children: const <Widget>[
-                  CardItem(),
-                  CardItem(),
-                  CardItem(),
-                  CardItem(),
-                  CardItem(),
-                  CardItem(),
-                  CardItem(),
-                ],
-              )),
-          const SizedBox(height: 15.0)
-        ],
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(50),
+        child: CustomAppBar(
+          title: "Items list",
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(height: 15.0),
+              SizedBox(
+                  width: MediaQuery.of(context).size.width - 30.0,
+                  height: MediaQuery.of(context).size.height - 50.0,
+                  child: GridView.count(
+                    crossAxisCount: 2,
+                    primary: false,
+                    crossAxisSpacing: 10.0,
+                    mainAxisSpacing: 15.0,
+                    childAspectRatio: 0.8,
+                    children: const <Widget>[
+                      CardItem(),
+                      CardItem(),
+                      CardItem(),
+                      CardItem(),
+                      CardItem(),
+                      CardItem(),
+                      CardItem(),
+                    ],
+                  )),
+              const SizedBox(height: 15.0)
+            ],
+          ),
+        ),
       ),
     );
   }
