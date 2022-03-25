ff11c6fdab871f41caba8a3b2d18286ba37b27f3
Yahya Khodr <yhy97kh18@gmail.com>
2022-03-26 00:09:53 +0200

    Created linked the login screen with the backend

--- frontend/lib/views/screens/auth_screens/login_screen.dart
+++ frontend/lib/views/screens/auth_screens/login_screen.dart
@@ -1,4 +1,10 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:frontend/models/http_response.dart';
+import 'package:frontend/services/user_service.dart';
+import 'package:frontend/utils/utilities.dart';
+import 'package:frontend/utils/validators.dart';
 import 'package:frontend/views/screens/main_screen.dart';
 import 'package:frontend/views/widgets/rounded_button_widget.dart';
 import 'package:frontend/views/widgets/text_field_widget.dart';
@@ -12,39 +18,92 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   late String _email, _password;
-  final TextEditingController _emailControl = TextEditingController();
-  final TextEditingController _passwordControl = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  bool _isloading = false;
   @override
   Widget build(BuildContext context) {
+    void _loginUser() {
+      if (Validator.isValidEmail(_email) &&
+          Validator.isValidPassword(_password)) {
+        setState(() {
+          _isloading = true;
+        });
+        final Future<HTTPResponse<String>> response =
+            UserService().loginUser(_email, _password);
+
+        response.then((value) {
+          if (value.isSuccessful) {
+            // Utilities.fetchAndSaveUserInfo(res: value.data);
+            // setState(() {
+            //   _isloading = false;
+            // });
+            setState(() {
+              _isloading = false;
+            });
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const MainScreen(),
+              ),
+            );
+          } else {
+            setState(() {
+              _isloading = false;
+              Utilities.showSnackbar(context, value.message, false);
+            });
+          }
+        });
+      } else if (!Validator.isValidEmail(_email)) {
+        Utilities.showSnackbar(context, 'Invalid  Email', false);
+      } else if (!Validator.isValidPassword(_password)) {
+        Utilities.showSnackbar(context, 'Password is empty', false);
+      }
+    }
+
     Size size = MediaQuery.of(context).size;
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(
-              height: size.height * 0.03,
-            ),
-            const CustomTextField(hintText: "Email"),
-            SizedBox(
-              height: size.height * 0.03,
-            ),
-            const CustomTextField(hintText: "Password"),
-            SizedBox(
-              height: size.height * 0.03,
-            ),
-            RoundedButton(
-              text: "Login",
-              onPressed: () => Navigator.of(context).pushReplacement(
-                MaterialPageRoute(
-                  builder: (context) => const MainScreen(),
-                ),
+    return _isloading
+        ? const Center(
+            child: CircularProgressIndicator(),
+          )
+        : SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    height: size.height * 0.03,
+                  ),
+                  CustomTextField(
+                    hintText: "Email",
+                    controller: _emailController,
+                  ),
+                  SizedBox(
+                    height: size.height * 0.03,
+                  ),
+                  CustomTextField(
+                    hintText: "Password",
+                    controller: _passwordController,
+                  ),
+                  SizedBox(
+                    height: size.height * 0.03,
+                  ),
+                  RoundedButton(
+                      text: "Login",
+                      onPressed: () {
+                        setState(() {
+                          _email = _emailController.text;
+                          _password = _passwordController.text;
+                        });
+                        log("email " + _email);
+                        log("password " + _password);
+                        _loginUser();
+                      }),
+                ],
               ),
             ),
-          ],
-        ),
-      ),
-    );
+          );
   }
 }
