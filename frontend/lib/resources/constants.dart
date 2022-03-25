09266087d0a3be4378364f0f934a230935ec82ee
Yahya Khodr <yhy97kh18@gmail.com>
2022-03-25 20:48:49 +0200

    Created strings for api endpoints

--- frontend/lib/resources/constants.dart
+++ frontend/lib/resources/constants.dart
@@ -1,4 +1,9 @@
 class Constants {
   static String appName = "PantryPal";
-  
+
+  static String api = "http://localhost:8000/api/";
+
+  static String loginUrl = api + "login/";
+  static String registerUrl = api + "register/";
+
 }
