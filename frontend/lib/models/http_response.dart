62d9066250dc5e48c955c238ca1bfa867ea064f8
Yahya Khodr <yhy97kh18@gmail.com>
2022-03-25 20:52:04 +0200

    Created model for http response

---
+++ frontend/lib/models/http_response.dart
@@ -0,0 +1,18 @@
+
+class HTTPResponse<T> {
+  bool isSuccessful;
+  T data;
+  String message;
+  int responseCode;
+
+  HTTPResponse(this.isSuccessful, this.data, this.message, this.responseCode);
+
+  factory HTTPResponse.fromJson(Map<String, dynamic> json) {
+    return HTTPResponse(
+      json['isSuccessful'],
+      json['data'],
+      json['message'],
+      json['responseCode'],
+    );
+  }
+}
