41191ab15d6ab889edfccbdb7f5c30c1d4eeb3c9
Yahya Khodr <yhy97kh18@gmail.com>
2022-03-26 16:56:55 +0200

    Created the viewmodel for the product

--- frontend/lib/viewmodels/product_viewmodel.dart
+++ frontend/lib/viewmodels/product_viewmodel.dart
@@ -1,6 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:frontend/models/nutriment_model.dart';
+import 'package:frontend/models/product_model.dart';
+import 'package:frontend/services/product_service.dart';
 
+class ProductViewModel extends ChangeNotifier {
+  final ProductService _productService = ProductService();
+  Product? _product;
+  Product? get product => _product;
 
-class ProductViewModel {
+  Nutriments? _nutriments;
+  Nutriments? get nutriments => _nutriments;
 
-  //To be implemented
+  setNutriments(Nutriments nutriments) {
+    _nutriments = nutriments;
+  }
+
+  setProduct(Product product) {
+    _product = product;
+  }
+
+  fetchProduct(String barcode) async {
+    var response = await _productService.fetchProduct(barcode);
+    if (response.status == 1) {
+      setProduct(response.product as Product);
+      setNutriments(response.product?.nutriments as Nutriments);
+    }
+  }
 }
