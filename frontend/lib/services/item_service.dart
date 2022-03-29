0833adbc11e8b28338ff4439ab54df9d9031018b
Yahya Khodr <yhy97kh18@gmail.com>
2022-03-29 13:24:23 +0300

    Implemented the Item service

--- frontend/lib/services/item_service.dart
+++ frontend/lib/services/item_service.dart
@@ -1,12 +1,13 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:frontend/models/http_response.dart';
+import 'package:frontend/models/item_model.dart';
 import 'package:frontend/resources/constants.dart';
 import "package:http/http.dart" as http;
 
 class ItemService {
-  static Future<bool> addItem(String token, String barcode, DateTime expiryDate,
-      DateTime purchasedDate) async {
+  static Future<bool> addItem(String token, Item item) async {
     Uri url = Uri.parse(Constants.addItemUrl);
     try {
       final response = await http.post(
@@ -15,11 +16,7 @@ class ItemService {
           "Content-Type": "application/json",
           "Authorization": "Token " + token,
         },
-        body: json.encode({
-          "barcode": barcode,
-          "expiry_date": expiryDate,
-          "purchasedDate": purchasedDate,
-        }),
+        body: json.encode(item.toJson()),
       );
       if (response.statusCode == 200) {
         return true;
@@ -32,6 +29,33 @@ class ItemService {
       return false;
     } catch (e) {
       return false;
+    }
+  }
+
+  static Future<HTTPResponse<List<Item>?>> getItems(String token) async {
+    Uri url = Uri.parse(Constants.getItemsUrl);
+
+    try {
+      final response = await http.get(
+        url,
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Token " + token,
+        },
+      );
+      if (response.statusCode == 200) {
+        return HTTPResponse(true, itemsListModelFromJson(response.body),
+            "Success", response.statusCode);
+      } else {
+        return HTTPResponse(false, jsonDecode(response.body),
+            "Failed to get items", response.statusCode);
+      }
+    } on SocketException {
+      return HTTPResponse(false, null, "No Internet connection", 400);
+    } on FormatException {
+      return HTTPResponse(false, null, "Invalid Response", 400);
+    } catch (e) {
+      return HTTPResponse(false, null, "Error occured", 400);
     }
   }
 }
