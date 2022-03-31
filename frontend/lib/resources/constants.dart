d56afdd0ef2958de365da666a3db792948280d9e
Yahya Khodr <yhy97kh18@gmail.com>
2022-03-31 17:01:46 +0300

    Added food's endpoints

--- frontend/lib/resources/constants.dart
+++ frontend/lib/resources/constants.dart
@@ -2,6 +2,8 @@ class Constants {
   static String appName = "PantryPal";
 
   static String api = "http://192.168.8.202:8000/api/";
+  static String imageApi = "http://192.168.8.202:8000";
+  // static String api = "http://10.0.2.2:8000/api/";
 
   static String loginUrl = api + "login/";
   static String registerUrl = api + "register/";
@@ -11,8 +13,24 @@ class Constants {
   static String updateProfileImageUrl = api + "update_profile_image/";
 
   static String addProductUrl = api + "add_product/";
-  static String getProduct = api + "product/";
-  static String getProductByCategory = api + "product_by_category/";
+  static String getProductUrl = api + "product/";
+  static String getProductByCategory =
+      api + "product_by_category/"; // takes category as parameter
+
+
+    
+  static String addFoodUrl = api + "add_food/";
+  static String getFoodsUrl = api + "get_foods/";
+  static String getFoodByCategoryUrl = api + "get_food_by_category/"; // takes category as parameter
+  static String removeFoodUrl = api + "remove_food/"; // takes food Id as parameter
+  static String increaseFood = api + "increase_quantity/";
+  static String decreaseFood = api + "decrease_quantity/";
+      
+
+  static String addItemUrl = api + "add_item/";
+  static String getItemsUrl = api + "get_items/";
+  static String removeItemUrl =
+      api + "remove_item/"; // takes item id as parameter
 
   static String remoteApi = "https://world.openfoodfacts.org/api/v0/product/";
 }
