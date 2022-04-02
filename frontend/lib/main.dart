08603671483319d80f7a4fc585e856bf89434c44
Yahya Khodr <yhy97kh18@gmail.com>
2022-04-02 13:45:16 +0300

    Implemented future builder to check token

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,33 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/resources/palette.dart';
+import 'package:frontend/viewmodels/food_viewmodel.dart';
 import 'package:frontend/viewmodels/product_viewmodel.dart';
+import 'package:frontend/viewmodels/user_viewmodel.dart';
 import 'package:frontend/views/screens/auth_screens/auth_screen.dart';
 import 'package:frontend/views/screens/main_screen.dart';
-
-import 'package:frontend/views/screens/update_profile_screens/update.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final UserViewModel userViewModel = UserViewModel();
+    final FoodViewModel foodViewModel = FoodViewModel();
+
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider(create: (_) => FoodViewModel()),
+        ChangeNotifierProvider(create: (_) => UserViewModel()),
         ChangeNotifierProvider(create: (_) => ProductViewModel()),
       ],
       child: MaterialApp(
-        title: 'Pantry Tracker',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-            primarySwatch: Colors.blue,
-            scaffoldBackgroundColor: Palette.backgroundColor),
-        home: const MainScreen(),
-      ),
+          title: 'Pantry Tracker',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+              primarySwatch: Colors.blue,
+              scaffoldBackgroundColor: Palette.backgroundColor),
+          home: FutureBuilder<String?>(
+            future: foodViewModel.getUserToken(),
+            builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
+              if (snapshot.connectionState != ConnectionState.done) {
+                return const CircularProgressIndicator();
+              }
+              if (snapshot.hasData) {
+                return const MainScreen();
+              }
+              return const Authentication();
+            },
+          )),
     );
   }
 }
