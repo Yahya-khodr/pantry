5b36cf2a745932123e44652b7dbe1d9d609dd3d5
Yahya Khodr <yhy97kh18@gmail.com>
2022-04-02 16:08:33 +0300

    Implmented edit user info tab

--- frontend/lib/views/screens/update_profile_screens/update_user_info.dart
+++ frontend/lib/views/screens/update_profile_screens/update_user_info.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/resources/palette.dart';
+import 'package:frontend/models/user_model.dart';
 import 'package:frontend/views/widgets/rounded_button_widget.dart';
 import 'package:frontend/views/widgets/text_field_widget.dart';
 
 class UpdateUserInfo extends StatefulWidget {
-  const UpdateUserInfo({Key? key}) : super(key: key);
+  final User user;
+  final String token;
+  const UpdateUserInfo({
+    Key? key,
+    required this.user,
+    required this.token,
+  }) : super(key: key);
 
   @override
   State<UpdateUserInfo> createState() => _UpdateUserInfoState();
@@ -17,11 +23,33 @@ class _UpdateUserInfoState extends State<UpdateUserInfo> {
   final TextEditingController _emailController = TextEditingController();
 
   @override
+  void initState() {
+    super.initState();
+    initializeData();
+  }
+
+  void initializeData() {
+    _nameController.value = _nameController.value.copyWith(
+      text: widget.user.name,
+      selection: widget.user.name != null
+          ? TextSelection.collapsed(offset: widget.user.name!.length)
+          : null,
+    );
+
+    _emailController.value = _emailController.value.copyWith(
+      text: widget.user.email,
+      selection: widget.user.email != null
+          ? TextSelection.collapsed(offset: widget.user.email!.length)
+          : null,
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: Column(
+      child: ListView(
         children: [
           const SizedBox(height: 20.0),
           CustomTextField(
@@ -36,10 +64,15 @@ class _UpdateUserInfoState extends State<UpdateUserInfo> {
             labelText: "Email Address",
           ),
           const SizedBox(height: 20.0),
-          RoundedButton(
-            text: "Save",
-            onPressed: () {},
+          Container(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
             width: size.width * 0.3,
+            child: RoundedButton(
+              text: "Save",
+              onPressed: () {},
+              width: size.width * 0.3,
+            ),
           ),
         ],
       ),
