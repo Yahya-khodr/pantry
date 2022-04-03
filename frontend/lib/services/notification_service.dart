5df6467e9eb65a26b5240268d1ae62f34c0bfc9e
Yahya Khodr <yhy97kh18@gmail.com>
2022-04-04 00:22:42 +0300

    Implmeneted local notifications service

---
+++ frontend/lib/services/notification_service.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:rxdart/rxdart.dart';
+
+class NotificationService {
+  static final _notifications = FlutterLocalNotificationsPlugin();
+  static final onNotification = BehaviorSubject<String?>();
+
+  static Future _notificationDetails() async {
+    return const NotificationDetails(
+      android: AndroidNotificationDetails('channel id', 'channel name',
+          importance: Importance.max),
+      iOS: IOSNotificationDetails(),
+    );
+  }
+
+  static Future init({bool initScheduled = false}) async {
+    const android = AndroidInitializationSettings('@mimap/ic_launcher');
+    const iOS = IOSInitializationSettings();
+    final settings = InitializationSettings(android: android, iOS: iOS);
+    await _notifications.initialize(
+      settings,
+      onSelectNotification: (payload) async {
+        onNotification.add(payload);
+      },
+    );
+  }
+
+  static Future showNotification({
+    int id = 0,
+    String? title,
+    String? body,
+    String? payLoad,
+  }) async =>
+      _notifications.show(id, title, body, await _notificationDetails());
+}
