3d59ad7ca225befd001d0b97317317d19922bd63
Yahya Khodr <yhy97kh18@gmail.com>
2022-04-07 12:32:47 +0300

    Added extra colors

--- frontend/lib/resources/palette.dart
+++ frontend/lib/resources/palette.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class Palette {
@@ -10,5 +9,15 @@ class Palette {
   static const Color textFieldColor = Color(0xFFFFFFFF);
   static const Color buttonColor = Color(0xFF00af54);
   static const Color buttonColorRed = Colors.red;
-  static const Color TextColor = Color(0xFF767676);
+  static const Color textColor = Color(0xFF767676);
+  static const Color blackColor = Color(0xff19191b);
+
+  static const Color gradientColorStart = Color(0xFF00AF54);
+  static const Color gradientColorEnd = Color(0xFF0EB177);
+  static const Color separatorColor = Color(0xff272c35);
+
+  static const Gradient fabGradient = LinearGradient(
+      colors: [gradientColorStart, gradientColorEnd],
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight);
 }
