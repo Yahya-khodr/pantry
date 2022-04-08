a7957de7133adfd08a9bbdbfe5f5e5495d615aed
Yahya Khodr <yhy97kh18@gmail.com>
2022-04-08 18:04:44 +0300

    Fix slider item widget

--- frontend/lib/views/widgets/slider_item.dart
+++ frontend/lib/views/widgets/slider_item.dart
@@ -1,40 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/resources/palette.dart';
 
 class SliderItem extends StatelessWidget {
   const SliderItem({
     Key? key,
     this.color = Colors.green,
-    this.text = 'Fruits & Vegetables  ',
+    required this.image,
+    required this.text,
   }) : super(key: key);
 
   final Color color;
   final String text;
-
+  final String image;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(color: const Color(0xffeeeeee), width: 2.0),
-        color: Colors.white38,
-        borderRadius: const BorderRadius.all(Radius.circular(8.0)),
-        boxShadow: const [
-          BoxShadow(
-            color: Colors.white10,
-            blurRadius: 4,
-            spreadRadius: 2,
-            offset: Offset(0, 2),
-          ),
-        ],
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(24),
       ),
-      margin: const EdgeInsets.all(8),
-      height: 200,
-      width: 200,
       child: Stack(
+        alignment: Alignment.center,
         children: [
-          Text(
-            text,
-            style: TextStyle(
-                fontWeight: FontWeight.bold, fontSize: 24.0, color: color),
+          Ink.image(
+            image: AssetImage(
+              image,
+            ),
+            colorFilter: const ColorFilter.mode(
+              Palette.appBarColor,
+              BlendMode.dst,
+            ),
+            child: InkWell(
+              onTap: () {},
+            ),
+            height: 240,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+                color: Colors.black38, borderRadius: BorderRadius.circular(10)),
+            child: Text(
+              text.toUpperCase(),
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Palette.textFieldColor,
+                fontSize: 24,
+              ),
+            ),
           ),
         ],
       ),
