1f756408a8c38633d51d256d958c136906b59d11
Yahya Khodr <yhy97kh18@gmail.com>
2022-04-08 18:00:25 +0300

    Added suggestions list to buy items

---
+++ frontend/lib/utils/suggestions.dart
@@ -0,0 +1,54 @@
+List suggestionsItems = [
+  {
+    'name': 'pasta',
+    'image': 'assets/items/pasta.jpg',
+  },
+  {
+    'name': 'butter',
+    'image': 'assets/items/butter.jpg',
+  },
+  {
+    'name': 'flour',
+    'image': 'assets/items/flour.jpg',
+  },
+  {
+    'name': 'lemon',
+    'image': 'assets/items/lemon.jpg',
+  },
+  {
+    'name': 'milk',
+    'image': 'assets/items/milk.jpg',
+  },
+  {
+    'name': 'potato',
+    'image': 'assets/items/potato.jpg',
+  },
+  {
+    'name': 'meat',
+    'image': 'assets/items/meat.jpg',
+  },
+  {
+    'name': 'egg',
+    'image': 'assets/items/egg.jpg',
+  },
+  {
+    'name': 'rice',
+    'image': 'assets/items/rice.jpg',
+  },
+  {
+    'name': 'tomato',
+    'image': 'assets/items/tomato.jpg',
+  },
+  {
+    'name': 'sugar',
+    'image': 'assets/items/sugar.jpg',
+  },
+  {
+    'name': 'cheese',
+    'image': 'assets/items/white_cheese.jpg',
+  },
+  {
+    'name': 'almond',
+    'image': 'assets/items/almonde.jpg',
+  },
+];
