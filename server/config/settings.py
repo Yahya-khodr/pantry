0d404503bcc147083ef5c3759e5183555c7c6138
Yahya Khodr <yhy97kh18@gmail.com>
2022-04-10 15:23:02 +0300

    Fix expiry date in recent items

--- server/config/settings.py
+++ server/config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY')),
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.21.227.244']
+ALLOWED_HOSTS = ['3.21.227.244', '192.168.8.202']
 
 
 # Application definition
